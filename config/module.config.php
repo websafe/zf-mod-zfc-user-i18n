adc78be8859e387b00aa6cd8b27ee8fb49f21e30
Thomas Szteliga <ts@websafe.pl>
2013-11-11 04:46:13 +0100

    Testbuild.

--- config/module.config.php
+++ config/module.config.php
@@ -7,17 +7,17 @@ return array(
         'translation_files' => array(
             array(
                 'type'     => 'gettext',
-                'filename' => __DIR__ . '/../../zf-mod-zfc-user-i18n-de-de/language/de_DE.mo',
+                'filename' => __DIR__ . '/../language/de_DE.mo',
                 'locale'   => 'de_DE'
             ),
             array(
                 'type'     => 'gettext',
-                'filename' => __DIR__ . '/../../zf-mod-zfc-user-i18n-ja-jp/language/ja_JP.mo',
+                'filename' => __DIR__ . '/../language/ja_JP.mo',
                 'locale'   => 'ja_JP'
             ),
             array(
                 'type'     => 'gettext',
-                'filename' => __DIR__ . '/../../zf-mod-zfc-user-i18n-pl-pl/language/pl_PL.mo',
+                'filename' => __DIR__ . '/../language/pl_PL.mo',
                 'locale'   => 'pl_PL'
             ),
         ),
